90128702e7d74b0e72fadebafbc06be7d832f87c
Akshay <dhamakk@gmail.com>
2021-01-10 09:29:30 +1100

    Input and applied data in movie card

--- src/app/moviecard/moviecard.component.ts
+++ src/app/moviecard/moviecard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { dataType } from '../movie';
+
 
 @Component({
   selector: 'app-moviecard',
@@ -7,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviecardComponent implements OnInit {
 
+  @Input() movie: dataType;
+
+  imageurl = 'https://image.tmdb.org/t/p/w92/{{movie.poster_path}}'; 
   constructor() { }
 
   ngOnInit(): void {
