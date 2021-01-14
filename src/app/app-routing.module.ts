2e72a9a38b326f8eab9234895df28a90e79d93b4
Akshay <dhamakk@gmail.com>
2021-01-14 13:44:57 +1100

    Added defualt router path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {MovieDetailComponent} from './movie-detail/movie-detail.component'
-import {PageNotFoundComponent} from './page-not-found/page-not-found.component'
+import {MovieDetailComponent} from './movie-detail/movie-detail.component';
+import {SearchComponent} from './search/search.component';
+import {MoviecardListComponent} from './moviecard-list/moviecard-list.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  {path: '', component:MoviecardListComponent},
   {path: 'movie/:id', component:MovieDetailComponent},
+  {path: 'search/:emotion', component:SearchComponent},
   {path: '**', component:PageNotFoundComponent}
 
 ];
