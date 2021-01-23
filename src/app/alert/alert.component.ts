927b984790f9908bac5c7b35f5ecef7f28d5df9f
Akshay <dhamakk@gmail.com>
2021-01-23 14:02:37 +1100

    Added effect to pop-up alert box

--- src/app/alert/alert.component.ts
+++ src/app/alert/alert.component.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import {InMemoryDataService} from '../service/in-memory-data.service';
 import { Router } from '@angular/router';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+
 
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.css']
+  styleUrls: ['./alert.component.css'],
+  animations:[
+    trigger('boxState', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0) scale(1)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateY(0) scale(0.5)'}
+        ),
+        animate(400)
+      ]),
+  ])
+]
 })
 export class AlertComponent implements OnInit {
 
